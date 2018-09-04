b5a52b55c221ffb95be77cea8bf51d1f77a6016d
Rasmus <rallemm96@gmail.com>
2018-09-04 20:48:44 +0200

    fixed urls

--- server.py
+++ server.py
@@ -8,11 +8,10 @@ from firebase_admin import messaging
 from firebase_admin import db
 
 urls = [
-    "https://www.jv.dk/rss/nyheder"
-    # "https://www.tipsbladet.dk/danmark/esbjerg-fb-male/feed",
-    # "https://www.jv.dk/rss/efb",
-    # "https://www.bold.dk/feed/rss_by_tag/8467/",
-    # "https://www.efb.dk/feed/"
+    "https://www.tipsbladet.dk/danmark/esbjerg-fb-male/feed",
+    "https://www.jv.dk/rss/efb",
+    "https://www.bold.dk/feed/rss_by_tag/8467/",
+    "https://www.efb.dk/feed/"
 ]
 
 links = []
